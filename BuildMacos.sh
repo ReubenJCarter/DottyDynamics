9b3e66211689995529d5bc7ba2e2ceeaa1a02651
Reuben Carter <reubenjcarter@gmail.com>
2023-02-08 17:43:24 +0000

    Added dylib ext

--- BuildMacos.sh
+++ BuildMacos.sh
@@ -1,3 +1,3 @@
 mkdir -p ./build/macos
-g++ -dynamiclib -fPIC -D UNIXLIKE -Ofast -std=c++17 -o ./build/macos/libDotty.so ./src/DottyBind.cpp
-cp ./build/macos/libDotty.so ./unity/Assets/Dotty/Plugins/MacOS
+g++ -dynamiclib -fPIC -D UNIXLIKE -Ofast -std=c++17 -o ./build/macos/libDotty.dylib ./src/DottyBind.cpp
+cp ./build/macos/libDotty.dylib ./unity/Assets/Dotty/Plugins/MacOS
