f12e8f4b77a3ecc676fe0bdeeedd5e82dd96a4de
Reuben Carter <reubenjcarter@gmail.com>
2023-02-08 17:28:29 +0000

    Added shared lib flag

--- BuildMacos.sh
+++ BuildMacos.sh
@@ -1,3 +1,3 @@
 mkdir -p ./build/macos
-g++ -shared -fPIC -D UNIXLIKE -Ofast -std=c++17 -o ./build/macos/libDotty.so ./src/DottyBind.cpp
+g++ -dynamiclib -fPIC -D UNIXLIKE -Ofast -std=c++17 -o ./build/macos/libDotty.so ./src/DottyBind.cpp
 cp ./build/macos/libDotty.so ./unity/Assets/Dotty/Plugins/MacOS
