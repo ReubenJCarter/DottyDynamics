86bb3c868dc3682328b4d21a5baeee8d9d60fa68
Reuben Carter <reubenjcarter@gmail.com>
2023-02-15 12:40:12 +0000

    Removed static flag

--- BuildIOS.sh
+++ BuildIOS.sh
@@ -2,6 +2,6 @@ mkdir -p ./build/ios
 
 `xcrun --sdk iphoneos --find clang` -Os  -isysroot `xcrun --sdk iphoneos \
   --show-sdk-path` -F`xcrun --sdk iphoneos --show-sdk-path`/System/Library/Frameworks \
-  -arch arm64 -static -D UNIXLIKE -Ofast -std=c++17 -v -o ./build/ios/libDotty.a ./src/DottyBind.cpp
+  -arch arm64 -D UNIXLIKE -Ofast -std=c++17 -v -o ./build/ios/libDotty.a ./src/DottyBind.cpp
 
 cp ./build/ios/libDotty.a ./unity/Assets/Dotty/Plugins/IOS
