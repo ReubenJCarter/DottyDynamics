16e364f0fc43980e0fe16700bc744100cff3185f
Reuben Carter <reubenjcarter@gmail.com>
2023-02-15 12:55:22 +0000

    Added correct folder

--- BuildIOS.sh
+++ BuildIOS.sh
@@ -4,6 +4,6 @@ mkdir -p ./build/ios
   --show-sdk-path` -F`xcrun --sdk iphoneos --show-sdk-path`/System/Library/Frameworks \
   -arch arm64 -D UNIXLIKE -Ofast -std=c++17 -v -c -fPIC -o ./build/ios/libDotty.o ./src/DottyBind.cpp
 
-`xcrun --sdk iphoneos --find ar` r libDotty.a ./build/ios/libDotty.o
+`xcrun --sdk iphoneos --find ar` r ./build/ios/libDotty.a ./build/ios/libDotty.o
 
 cp ./build/ios/libDotty.a ./unity/Assets/Dotty/Plugins/IOS
